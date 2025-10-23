04fc0e3cd525226ba903f134ed443adb15567afd
bryans-123 <bryan_steven.gomez@uao.edu.co>
2025-10-23 17:52:50 -0500

    Update database credentials in db.js

--- db.js
+++ db.js
@@ -5,8 +5,8 @@ require('dotenv').config();
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'sigeu_database',
+  password: process.env.DB_PASSWORD || 'root',
+  database: process.env.DB_NAME || 'sigeu_db',
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
